c93fef82de00178f600b5297770b1013ba8ee54e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-06-30 13:19:16 +0000

    RoseLoverX: auto linting

--- ryella/modules/downloads.py
+++ ryella/modules/downloads.py
@@ -59,8 +59,14 @@ async def _ls(message):
             dir_contents += "<code>📃 {} </code>(<code>{}</code>)\n".format(
                 con, human_readable_size(size)
             )
-    dir_contents += '\n'
-    dir_contents += '<code>Total Folders: {} {}'.format(folders[0], human_readable_size(folders[1]))
-    dir_contents += '<code>Total Files: {} {}'.format(files[0], human_readable_size(files[1]))
-    dir_contents += '<code>Total: {} {}</code>'.format(files[0] + folders[0], human_readable_size(files[1] + folders[1]))
+    dir_contents += "\n"
+    dir_contents += "<code>Total Folders: {} {}".format(
+        folders[0], human_readable_size(folders[1])
+    )
+    dir_contents += "<code>Total Files: {} {}".format(
+        files[0], human_readable_size(files[1])
+    )
+    dir_contents += "<code>Total: {} {}</code>".format(
+        files[0] + folders[0], human_readable_size(files[1] + folders[1])
+    )
     await message.edit(dir_contents)
