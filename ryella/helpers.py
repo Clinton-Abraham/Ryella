5f9996e626c9def30d11ab65b0ae8b11f4b63a6c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2022-08-01 16:04:41 +0000

    RoseLoverX: auto linting

--- ryella/helpers.py
+++ ryella/helpers.py
@@ -3,13 +3,14 @@ import importlib
 import logging
 import math
 import os
-import time
-import psutil
 import platform
-from datetime import datetime
-import socket
-import uuid
 import re
+import socket
+import time
+import uuid
+from datetime import datetime
+
+import psutil
 import pymongo
 import telethon
 from PIL import Image
@@ -166,6 +167,7 @@ def resize_image(image, width, height):
     image = image.resize((width, height), Image.ANTIALIAS)
     return image
 
+
 async def run_cmd(cmd):
     """Runs a shell command."""
     process = await asyncio.create_subprocess_shell(
@@ -174,9 +176,10 @@ async def run_cmd(cmd):
     stdout, stderr = await process.communicate()
     return stdout.decode(), stderr.decode()
 
+
 def system_information():
     """Get full system Specs."""
-    print("="*40, "System Information", "="*40)
+    print("=" * 40, "System Information", "=" * 40)
     uname = platform.uname()
     print(f"System: {uname.system}")
     print(f"Node Name: {uname.node}")
@@ -188,13 +191,13 @@ def system_information():
     print(f"Mac-Address: {':'.join(re.findall('..', '%012x' % uuid.getnode()))}")
 
     # Boot Time
-    print("="*40, "Boot Time", "="*40)
+    print("=" * 40, "Boot Time", "=" * 40)
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
     print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
 
     # print CPU information
-    print("="*40, "CPU Info", "="*40)
+    print("=" * 40, "CPU Info", "=" * 40)
     # number of cores
     print("Physical cores:", psutil.cpu_count(logical=False))
     print("Total cores:", psutil.cpu_count(logical=True))
@@ -210,7 +213,7 @@ def system_information():
     print(f"Total CPU Usage: {psutil.cpu_percent()}%")
 
     # Memory Information
-    print("="*40, "Memory Information", "="*40)
+    print("=" * 40, "Memory Information", "=" * 40)
     # get the memory details
     svmem = psutil.virtual_memory()
     print(f"Total: {get_size(svmem.total)}")
@@ -218,7 +221,7 @@ def system_information():
     print(f"Used: {get_size(svmem.used)}")
     print(f"Percentage: {svmem.percent}%")
 
-    print("="*20, "SWAP", "="*20)
+    print("=" * 20, "SWAP", "=" * 20)
     # get the swap memory details (if exists)
     swap = psutil.swap_memory()
     print(f"Total: {get_size(swap.total)}")
@@ -227,7 +230,7 @@ def system_information():
     print(f"Percentage: {swap.percent}%")
 
     # Disk Information
-    print("="*40, "Disk Information", "="*40)
+    print("=" * 40, "Disk Information", "=" * 40)
     print("Partitions and Usage:")
     # get all disk partitions
     partitions = psutil.disk_partitions()
@@ -251,17 +254,17 @@ def system_information():
     print(f"Total write: {get_size(disk_io.write_bytes)}")
 
     ## Network information
-    print("="*40, "Network Information", "="*40)
+    print("=" * 40, "Network Information", "=" * 40)
     ## get all network interfaces (virtual and physical)
     if_addrs = psutil.net_if_addrs()
     for interface_name, interface_addresses in if_addrs.items():
         for address in interface_addresses:
             print(f"=== Interface: {interface_name} ===")
-            if str(address.family) == 'AddressFamily.AF_INET':
+            if str(address.family) == "AddressFamily.AF_INET":
                 print(f"  IP Address: {address.address}")
                 print(f"  Netmask: {address.netmask}")
                 print(f"  Broadcast IP: {address.broadcast}")
-            elif str(address.family) == 'AddressFamily.AF_PACKET':
+            elif str(address.family) == "AddressFamily.AF_PACKET":
                 print(f"  MAC Address: {address.address}")
                 print(f"  Netmask: {address.netmask}")
                 print(f"  Broadcast MAC: {address.broadcast}")
@@ -269,4 +272,3 @@ def system_information():
     net_io = psutil.net_io_counters()
     print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
     print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
-
