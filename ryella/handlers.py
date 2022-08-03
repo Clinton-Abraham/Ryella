1ebe2276230788944f5c2872c2ffe34d168ac2e1
Amarnath C <amarnathcharichilil@gmail.com>
2022-08-03 13:58:22 +0530

    Update handlers.py

--- ryella/handlers.py
+++ ryella/handlers.py
@@ -11,7 +11,7 @@ def user_cmd(pattern: str, _help: str = ""):
     """
     Decorator for user commands.
     """
-    pattern = "^(?i)" + "[.,]" + pattern + "?(?: |$)"
+    pattern = "^(?i)" + "[,*]" + pattern + "?(?: |$)"
 
     def decorator(func):
         async def wrapper(message):
