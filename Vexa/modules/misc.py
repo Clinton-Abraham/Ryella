78cf9189072a0515a6a477c291df79761e89ed94
Amarnath C <amarnathcharichilil@gmail.com>
2022-08-18 19:02:12 +0530

    Update misc.py

--- Vexa/modules/misc.py
+++ Vexa/modules/misc.py
@@ -1,1 +1,21 @@
-.
+from ..handlers import user_cmd
+from requests import get
+from ..helpers import get_text_content
+
+@user_cmd(pattern="math")
+async def math(message):
+    exp = await get_text_content(message)
+    if exp is None:
+        return await message.reply("No expression provided!")
+    url = "https://evaluate-expression.p.rapidapi.com"
+    headers = {
+        "x-rapidapi-host": "evaluate-expression.p.rapidapi.com",
+        "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+    }
+    params = {"expression": exp}
+    response = get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        return await message.reply("Error: {}".format(response.status_code))
+    result = response.text
+    result = "ERR73" if result == "" else result
+    await message.edit("**► MathExp**\n**`{}`\n\n**► RESULT**\n`{}`".format(exp, result))
